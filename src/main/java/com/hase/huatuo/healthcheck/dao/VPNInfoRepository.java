887692bd145f3af0af19f8b7cad42824bad82b28
davidgking <43306298+davidgking@users.noreply.github.com>
2020-02-01 17:17:45 +0800

    Revert "Function/remove lombok"

--- src/main/java/com/hase/huatuo/healthcheck/dao/VPNInfoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hase.huatuo.healthcheck.dao;
-
-import com.hase.huatuo.healthcheck.model.VPNStateInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VPNInfoRepository extends JpaRepository<VPNStateInfo,String> {
-
-}
